1898d82729c87891cbd3ca1f10fbc8d4dcfd4763
OnlyYellow <ash1823@naver.com>
2024-01-31 17:26:22 +0900

    feat: 청원 리스트/청원 상세보기 모델 생성

--- petitions/models.py
+++ petitions/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Petition_Detail(models.Model):
+    BILL_NO = models.CharField(primary_key=True, null=False, blank=False)
+    APPROVER = models.CharField(max_length=30, null=False, blank=False)
+    CURR_COMMITTEE = models.CharField(max_length=30, null=False, blank=False)
+    LINK_URL = models.CharField(max_length=255, null=False, blank=False)
+    petition_file = models.FileField()
+    
+    def __str__(self):
+        return self.BILL_NO
+    
+    
+class Petition(models.Model):
+    BILL_NO = models.OneToOneField(Petition_Detail, primary_key=True, null=False, blank=False, on_delete=models.CASCADE)
+    BILL_NAME = models.CharField(max_length=100, null=False, blank=True)
+    PROPOSER = models.CharField(max_length=30, null=False, blank=True)
+    PROPOSER_DT = models.DateField(null=False, blank=False)
+    COMMITTEE_DT = models.DateField(null=False, blank=False)
+    content = models.TextField(null=False)
+    
+    def __str__(self):
+        return self.BILL_NAME
