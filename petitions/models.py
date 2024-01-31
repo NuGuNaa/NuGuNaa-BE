61a1adaa0f077ad9bd6291195802b49333802244
OnlyYellow <ash1823@naver.com>
2024-02-01 03:39:46 +0900

    fix: 데이터 정규화 처리 - 파일 분리

--- petitions/models.py
+++ petitions/models.py
@@ -6,19 +6,26 @@ class Petition_Detail(models.Model):
     APPROVER = models.CharField(max_length=30, null=False, blank=False)
     CURR_COMMITTEE = models.CharField(max_length=30, null=False, blank=False)
     LINK_URL = models.CharField(max_length=255, null=False, blank=False)
-    petition_file = models.FileField(upload_to="")
     
     def __str__(self):
         return self.BILL_NO
     
     
+class Petition_File(models.Model):
+    BILL_NO = models.CharField(primary_key=True, max_length=30, null=False, blank=False)
+    petition_file = models.FileField(upload_to="")
+    content = models.TextField(null=False)
+    
+    def __str__(self):
+        return self.content
+    
+
 class Petition(models.Model):
-    BILL_NO = models.OneToOneField(Petition_Detail, primary_key=True, max_length=30, null=False, blank=False, on_delete=models.CASCADE)
+    BILL_NO = models.OneToOneField(Petition_Detail, Petition_File, primary_key=True, max_length=30, null=False, blank=False, on_delete=models.CASCADE)
     BILL_NAME = models.CharField(max_length=100, null=False, blank=True)
     PROPOSER = models.CharField(max_length=30, null=False, blank=True)
     PROPOSER_DT = models.DateField(null=False, blank=False)
     COMMITTEE_DT = models.DateField(null=False, blank=False)
-    content = models.TextField(null=False)
     
     def __str__(self):
         return self.BILL_NAME
