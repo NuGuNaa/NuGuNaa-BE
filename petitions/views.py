0a0f4354b63727730018b95c045dcf14ea04e31e
OnlyYellow <ash1823@naver.com>
2024-01-31 17:50:20 +0900

    fix: 공공 api key 받아오기준비

--- petitions/views.py
+++ petitions/views.py
@@ -14,3 +14,7 @@ from rest_framework import permissions
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
 
+# 청원 리스트 보여주기
+class PetitionList(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+    authentication_classes = [JWTAuthentication]
