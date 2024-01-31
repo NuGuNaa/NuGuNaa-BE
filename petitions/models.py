e0416bd0221de1e15aa662d79e443fd0bf12b7fe
OnlyYellow <ash1823@naver.com>
2024-02-01 03:23:30 +0900

    fix: 청원 model 자료형 변경

--- petitions/models.py
+++ petitions/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Petition_Detail(models.Model):
-    BILL_NO = models.CharField(primary_key=True, null=False, blank=False)
+    BILL_NO = models.CharField(primary_key=True, max_length=30, null=False, blank=False)
     APPROVER = models.CharField(max_length=30, null=False, blank=False)
     CURR_COMMITTEE = models.CharField(max_length=30, null=False, blank=False)
     LINK_URL = models.CharField(max_length=255, null=False, blank=False)
@@ -13,7 +13,7 @@ class Petition_Detail(models.Model):
     
     
 class Petition(models.Model):
-    BILL_NO = models.OneToOneField(Petition_Detail, primary_key=True, null=False, blank=False, on_delete=models.CASCADE)
+    BILL_NO = models.OneToOneField(Petition_Detail, primary_key=True, max_length=30, null=False, blank=False, on_delete=models.CASCADE)
     BILL_NAME = models.CharField(max_length=100, null=False, blank=True)
     PROPOSER = models.CharField(max_length=30, null=False, blank=True)
     PROPOSER_DT = models.DateField(null=False, blank=False)
