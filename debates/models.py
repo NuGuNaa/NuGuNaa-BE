8d727c03830dfc46ccb3b7c8a67aad9ed2fe2371
OnlyYellow <ash1823@naver.com>
2024-02-02 06:49:58 +0900

    feat: 파일 텍스트화 및 debate app 생성

---
+++ debates/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from petitions.models import *
+
+
+class Debate(models.Model):
+    id = models.AutoField(primary_key=True, null=False, blank=False)
+    member_announcement_date = models.DateField(null=False, blank=False)
+    debate_date = models.DateTimeField(null=False, blank=False)
+    estimated_time = models.CharField(max_length=10, null=False, blank=False)
+    petition_id = models.OneToOneField(Petition, null=False, blank=False, on_delete=models.CASCADE)
+    debate_code_O = models.CharField(max_length=30, null=False, blank=False)
+    debate_code_X = models.CharField(max_length=30, null=False, blank=False)
+    
+    
