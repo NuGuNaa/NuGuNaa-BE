e9320896112ba26cca2af5fcd1d7213e7e07b2ef
OnlyYellow <ash1823@naver.com>
2024-02-06 06:20:21 +0900

    fix: 추첨 날짜 자료형 수정

--- debates/models.py
+++ debates/models.py
@@ -5,7 +5,7 @@ from accounts.models import *
 
 class Debate(models.Model):
     id = models.AutoField(primary_key=True, null=False, blank=False)
-    member_announcement_date = models.DateField(null=False, blank=False)
+    member_announcement_date = models.DateTimeField(null=False, blank=False)
     debate_date = models.DateTimeField(null=False, blank=False)
     estimated_time = models.CharField(max_length=10, null=False, blank=False, default="1시간")
     petition_id = models.OneToOneField(Petition, null=False, blank=False, on_delete=models.CASCADE)
