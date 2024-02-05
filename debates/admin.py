a15acf1cd7de1cdaff10d55783cb1a44a32bd1ea
OnlyYellow <ash1823@naver.com>
2024-02-06 06:27:21 +0900

    feat: debate 관리자 페이지

--- debates/admin.py
+++ debates/admin.py
@@ -2,8 +2,13 @@ from django.contrib import admin
 from .models import *
 
 
+class DebateCheck(admin.ModelAdmin):
+    list_display = ('id', 'petition_id', 'member_announcement_date', 'debate_date', 'debate_code_O', 'debate_code_X')
+
+
+
 class DebateApplyCheck(admin.ModelAdmin):
     list_display = ('id', 'petition_id', 'email', 'position', 'raffle_check')
 
-admin.site.register(Debate)
+admin.site.register(Debate, DebateCheck)
 admin.site.register(Debate_Apply, DebateApplyCheck)
