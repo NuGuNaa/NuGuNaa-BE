eedb1572b4938eff55d06dbfaf23a5baa0a930ad
OnlyYellow <ash1823@naver.com>
2024-02-06 06:27:36 +0900

    feat: debate 관리자 페이지

--- debates/admin.py
+++ debates/admin.py
@@ -6,7 +6,6 @@ class DebateCheck(admin.ModelAdmin):
     list_display = ('id', 'petition_id', 'member_announcement_date', 'debate_date', 'debate_code_O', 'debate_code_X')
 
 
-
 class DebateApplyCheck(admin.ModelAdmin):
     list_display = ('id', 'petition_id', 'email', 'position', 'raffle_check')
 
